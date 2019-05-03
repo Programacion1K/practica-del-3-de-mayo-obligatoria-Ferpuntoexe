2d8bca38d44af1eb8e0c763478dd540b1dd6d60a
kurur <kururu.fm@gmail.com>
2019-05-03 17:08:10 +0200

    Agenda-MAP

--- SRC/Agenda.java
+++ SRC/Agenda.java
@@ -1,2 +1,77 @@
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.PrintWriter;
+import java.util.*;
+import java.util.stream.Collectors;
+
 public class Agenda {
+    private String nombre;
+    private Map<Contacto, List<ItemDeInformacion>>listadecontactos=new Map<Contacto, List<ItemDeInformacion>>() {
+        @Override
+        public int size() {
+            return 0;
+        }
+
+        @Override
+        public boolean isEmpty() {
+            return false;
+        }
+
+        @Override
+        public boolean containsKey(Object key) {
+            return false;
+        }
+
+        @Override
+        public boolean containsValue(Object value) {
+            return false;
+        }
+
+        @Override
+        public List<ItemDeInformacion> get(Object key) {
+            return null;
+        }
+
+        @Override
+        public List<ItemDeInformacion> put(Contacto key, List<ItemDeInformacion> value) {
+            return null;
+        }
+
+        @Override
+        public List<ItemDeInformacion> remove(Object key) {
+            return null;
+        }
+
+        @Override
+        public void putAll(Map<? extends Contacto, ? extends List<ItemDeInformacion>> m) {
+
+        }
+
+        @Override
+        public void clear() {
+
+        }
+
+        @Override
+        public Set<Contacto> keySet() {
+            return null;
+        }
+
+        @Override
+        public Collection<List<ItemDeInformacion>> values() {
+            return null;
+        }
+
+        @Override
+        public Set<Entry<Contacto, List<ItemDeInformacion>>> entrySet() {
+            return null;
+        }
+    }
+
+
+
+
+
+
+    }
 }
--- SRC/Contacto.java
+++ SRC/Contacto.java
@@ -1,10 +1,12 @@
+import javax.swing.*;
+
 public class Contacto {
     private final String nombre;
-    private final int numero;
+    private final String numero;
     private final String email;
 
 
-    public Contacto(String nombre, int numero, String email) {
+    public Contacto(String nombre, String numero, String email) {
         this.nombre = nombre;
         this.numero = numero;
         this.email = email;
@@ -14,7 +16,7 @@ public class Contacto {
         return nombre;
     }
 
-    public int getNumero() {
+    public String getNumero() {
         return numero;
     }
 
@@ -22,4 +24,19 @@ public class Contacto {
         return email;
     }
 
+    @Override
+    public String toString() {
+        return "Contacto{" +
+                "nombre='" + nombre + '\'' +
+                ", numero='" + numero + '\'' +
+                ", email='" + email + '\'' +
+                '}';
+    }
+
+    public static void main(String[] args) {
+        Contacto contacto1=new Contacto( "John","117117117","JefeMaestro@UNSCmail.com");
+        Contacto contacto2=new Contacto( "Cortana","117117117","Cortana@UNSCmail.com");
+        System.out.println( contacto1 +"\n" +contacto2 );
+    }
 }
+
